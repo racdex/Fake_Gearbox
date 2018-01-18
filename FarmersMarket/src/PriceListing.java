04c56e3f26feb676843e36e9c8037caf776d3428
Alexandru Claudiu Rusu <acru@otn.com>
2018-01-18 10:51:15 +0100

    Farmer's market now sells apple.

--- FarmersMarket/src/PriceListing.java
+++ FarmersMarket/src/PriceListing.java
@@ -4,5 +4,6 @@ public class PriceListing {
 	public static void main(String[] args) {
 		System.out.println("Banana - 1$");
 		System.out.println("Pineapple - 3$");
+		System.out.println("Apples - 2$");
 	}
 }
