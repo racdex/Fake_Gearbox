70be02c544443820902b89b00e09e4e360c669a2
Alexandru Claudiu Rusu <acru@on.com>
2018-01-18 12:00:58 +0100

    Apple are out of stock

--- FarmersMarket/src/PriceListing.java
+++ FarmersMarket/src/PriceListing.java
@@ -4,7 +4,6 @@ public class PriceListing {
 	public static void main(String[] args) {
 		System.out.println("Banana - 1$");
 		System.out.println("Pineapple - 3$");
-		System.out.println("Apples - 2$");
 		System.out.println("Tomatos - 2$");
 	}
 }
