51a750334c8c20e6bd327d2aff8261ce2bcd5da8
Alexandru Claudiu Rusu <acru@on.com>
2018-01-18 13:12:03 +0100

    Some flour.

--- FarmersMarket/src/PriceListing.java
+++ FarmersMarket/src/PriceListing.java
@@ -5,5 +5,6 @@ public class PriceListing {
 		System.out.println("Banana - 1$");
 		System.out.println("Flower - 5$");
 		System.out.println("Peach - 2$");
+		System.out.println("Flour - 1$");
 	}
 }
