2249feca78b438c01812ea240eea201a9ecbe111
Alexandru Claudiu Rusu <acru@on.com>
2018-01-18 12:47:58 +0100

    Back to basics

--- FarmersMarket/src/PriceListing.java
+++ FarmersMarket/src/PriceListing.java
@@ -3,10 +3,5 @@ public class PriceListing {
 
 	public static void main(String[] args) {
 		System.out.println("Banana - 1$");
-		System.out.println("Pineapple - 3$");
-		System.out.println("Apples - 2$");
-		System.out.println("Tomatos - 2$");
-		System.out.println("Carrot - 2$");
-		System.out.println("Apple - 1$");
 	}
 }
